1298974c4c8633137636f0ceff3236dd6b611279
Julia BItencourt <minhajuliabitencourt500@gmail.com>
2025-03-25 16:31:05 -0300

    aula de concatenação

---
+++ aula02/concatenar.php
@@ -0,0 +1,14 @@
+<?php 
+  ECHO "Hello World!<br>";
+  echo "Hello World!<br>";
+  EcHo "Hello World!<br>";
+
+  $nome= "Julia";
+  $sobrenome= "Bitencourt";
+  $numero=31;
+
+  echo $nome . "Linda" . "<br>";
+  echo "${nome} <h1> ${sobrenome} </h1>";
+  echo $numero . "<p>" . $nome . " " . $sobrenome . "</p>";
+
+?>
